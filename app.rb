e8cb8c7398491fa3e09a62fc05a2e2e4cff1f2f1
taryn yaeger <tsyaeger@gmail.com>
2018-06-27 21:43:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,18 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+  get '/newteam' do
+    erb :team
+  end
+
+
+  post '/team' do
+    @name = params[:name]
+    
+    params.send("#{key}=", value)
+    
+    erb :newteam
+  end
+
+
 end
