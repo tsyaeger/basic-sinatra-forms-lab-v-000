8b1a55c3ca95653c99dc526961f92c191fe86c05
taryn yaeger <tsyaeger@gmail.com>
2018-06-27 22:20:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ class App < Sinatra::Base
 
   post '/team' do
     
-    # @name = params["name"]
-    # @coach = params["coach"]
-    # @pg = params["pg"]
-    # @sg = params["sg"]
-    # @sf = params["sf"]
-    # @pf = params["pf"]
-    # @center = params["c"]
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @center = params["c"]
 
     erb :team
   end
