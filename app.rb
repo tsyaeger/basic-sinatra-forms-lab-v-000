17c94b76cd4d967d8b8526dea8c9c5d702daa9ac
taryn yaeger <tsyaeger@gmail.com>
2018-06-27 22:11:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,11 +9,14 @@ class App < Sinatra::Base
 
 
   post '/team' do
-    @name = params[:name]
-    puts params
-    @team = params.reject! { |k| k == :name }
-    # params.send("#{key}=", value)
-    
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @center = params["c"]
+
     erb :team
   end
 
